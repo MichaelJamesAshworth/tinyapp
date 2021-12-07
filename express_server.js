d23cbc885bf1a36b554a17712c4fdb6e0fe598b6
Michael <Michael.James.Ashworth@Gmail.com>
2021-12-07 03:01:53 +0000

    Added Post for urls_news.ejs form

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -35,10 +38,19 @@ app.get("/set", (req, res) => {
    res.render('urls_index', templateVars);
  })
 
+ app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
  app.get("/urls/:shortURL", (req, res) => {
    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
    console.log(templateVars);
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Compass instructions are cryptic");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
