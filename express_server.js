70d4eaf0b42ad0e5cd38cb14b60c279b8b4d889f
Michael <Michael.James.Ashworth@Gmail.com>
2021-12-07 02:02:14 +0000

    fixed bug within urlDatabase. Implemented K to lowercase. line 9

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xk": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
@@ -29,6 +29,17 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+
+ app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+   res.render('urls_index', templateVars);
+ })
+
+ app.get("/urls/:shortURL", (req, res) => {
+   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+   console.log(templateVars);
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
