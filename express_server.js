f5a92e0280d51450a914d04df85824c2a507c165
Michael <Michael.James.Ashworth@Gmail.com>
2021-12-09 01:24:38 +0000

    Added post route for register page and implemented logic to display account email when logged in

--- express_server.js
+++ express_server.js
@@ -24,13 +24,18 @@ const urlDatabase = {
   "9sm5xk": "http://www.google.com"
 };
 
-//responsible for getting register page
-app.get('/register', (req, res) => {
-  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
-  res.render('register', templateVars)
-});
-
-// app.post()
+const userDatabase = {
+  "userID": {
+    id: "userID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "userID2": {
+    id: "userID2", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -40,32 +45,21 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
- 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
-
 app.get('/urls', (req, res) => {
-  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
+  userIDFromCookies = userDatabase[req.cookies["user_id"]];
+  const templateVars = { userIDFromCookies, urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
+  userIDFromCookies = userDatabase[req.cookies["user_id"]];
+  const templateVars = { userIDFromCookies, urls: urlDatabase };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req.body);
-  const templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  userIDFromCookies = userDatabase[req.cookies["user_id"]];
+  const templateVars = { userIDFromCookies, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
@@ -100,6 +94,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 });
 
+//
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString()
   urlDatabase[shortURL] = req.body.longURL;
@@ -107,6 +102,44 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+//responsible for getting register page
+app.get('/register', (req, res) => {
+  userIDFromCookies = userDatabase[req.cookies["user_id"]]
+  const templateVars = { userIDFromCookies, urls: urlDatabase };
+  res.render('register', templateVars)
+});
+
+app.post('/register', (req, res) => {
+  //extract the user info from the incoming form => req.body
+  const email = req.body.email;
+  const password = req.body.password;
+  //create a new user id => call our pre existing function
+  let userID = generateRandomString()
+  //add id, email, password to ours userdatabase => create a new user
+  const newUser = {
+      id: userID, 
+      email, 
+      password
+    }
+  // add the new user to the db
+  userDatabase[userID] = newUser;
+  //set the cookie => keep the userID in the cookie
+  res.cookie('user_id', userID);
+  console.log(userDatabase);
+  console.log(userID);
+  //redirect to '/urls'
+  res.redirect('/urls');
+});
+
+//lookup user object using the user_id cookie value
+
+for (let userID in userDatabase) {
+  const user = userDatabase[userID]; //retrieve the value
+
+}
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+userDatabase.userID.email
